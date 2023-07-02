8850dd2817f0ff4cb04b1f70125c95ae8b67f54d
Aidan Timson <contact@timmo.xyz>
2023-07-02 17:52:15 +0100

    Fix

--- rootfs/etc/cont-init.d/00-banner.sh
+++ rootfs/etc/cont-init.d/00-banner.sh
@@ -3,23 +3,15 @@
 # Container Base
 # Displays a simple banner on startup
 # ==============================================================================
-if bashio::supervisor.ping; then
-    bashio::log.blue \
-        '-----------------------------------------------------------'
-    bashio::log.blue " Debian Container Base"
-    bashio::log.blue " Debian Base image for containers"
-    bashio::log.blue \
-        '-----------------------------------------------------------'
+bashio::log.blue '-----------------------------------------------------------'
+bashio::log.blue " Debian Container Base"
+bashio::log.blue " Debian Base image for containers"
+bashio::log.blue '-----------------------------------------------------------'
 
-    bashio::log.blue " System: $(bashio::info.operating_system)" \
+bashio::log.blue " System: $(bashio::info.operating_system)" \
         " ($(bashio::info.arch) / $(bashio::info.machine))"
 
-    bashio::log.blue \
-        '-----------------------------------------------------------'
-    bashio::log.blue \
-        ' Please share the above information when looking for help'
-    bashio::log.blue \
-        ' or support.'
-    bashio::log.blue \
-        '-----------------------------------------------------------'
-fi
+bashio::log.blue '-----------------------------------------------------------'
+bashio::log.blue ' Please share the above information when looking for help'
+bashio::log.blue ' or support.'
+bashio::log.blue '-----------------------------------------------------------'
